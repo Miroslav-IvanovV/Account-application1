fb917e97355c513e06d010ef3e532247ae11b0c7
Miroslav-IvanovV <miroslavivanov002@gmail.com>
2021-08-05 11:34:16 +0300

    add AccountType class and did some logical changes on the account class

--- src/eu/deltasource/internship/accountapplication/Account.java
+++ src/eu/deltasource/internship/accountapplication/Account.java
@@ -1,17 +1,17 @@
-package eu.deltasource.internship;
+package eu.deltasource.internship.accountapplication;
 
 public class Account {
 
     private String iban;
-    private Type typeOfTheCard;
+    private AccountType cardType;
     private String owner;
     private double limit;
     private double currentState;
     private double interestRate;
 
-    public Account(String iban, Type type, String owner, double limit, double currentState, double interestRate) {
+    public Account(String iban, AccountType type, String owner, double limit, double currentState, double interestRate) {
         this.iban = iban;
-        this.typeOfTheCard = type;
+        this.cardType = type;
         this.owner = owner;
         this.limit = limit;
         this.currentState = currentState;
@@ -22,8 +22,9 @@ public class Account {
     /**
      * @return The characteristics of the account.
      */
-    public String accountCharacteristics() {
-        return "the iban is : " + iban + "\n" + "the type of the account is : " + typeOfTheCard + "\n"
+    @Override
+    public String toString() {
+        return "the iban is : " + iban + "\n" + "the type of the account is : " + cardType + "\n"
                 + "the owner of the account is : " + owner + "\n" + "the limit of the account is : " + limit + "\n"
                 + "the current state of the account is : " + currentState + "\n" + "the interst rate is : "
                 + interestRate + "\n";
@@ -32,40 +33,35 @@ public class Account {
     /**
      * @return The money that you owe to the bank.
      */
-    public double howMuchMoneyYouOwe(){
-        return currentState;
+    public double oweMoney() {
+        return limit - currentState;
     }
 
     /**
      * @return The owner of the card.
      */
-    public String cardOwner(){
+    public String getCardOwner() {
         return owner;
     }
 
     /**
      * @param payment is the money you put towards loan repayment.
      */
-    public void loanRepayment(double payment){
-        currentState -= payment;
+    public void repayLoan(double payment) {
+        currentState += payment;
         System.out.println("Your current balance is : " + currentState);
     }
 
     /**
      * @param withdraw is the money you want to withdraw form your credit card.
      */
-    public void withdrawMoney(double withdraw){
-        if(currentState + withdraw < limit) {
-            currentState += withdraw;
+    public void withdrawMoney(double withdraw) {
+        if (withdraw < currentState) {
+            currentState -= withdraw;
             System.out.println("Your current balance is : " + currentState);
         } else {
-            System.out.println("could not make the transaction youre going over your limit");
+            System.out.println("could not make the transaction you're going over your limit");
         }
     }
-
-    public enum Type {
-        CREDIT, DEBIT
-    }
-
 }
 
